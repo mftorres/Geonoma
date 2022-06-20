94b0bd7e705d484cdc5a12c365461c4f0fd295f0
MFTorresJ <mftorres27@gmail.com>
2022-06-20 14:41:47 +0300

    Update 01_mpileup.sh

--- PoolMaps/01_mpileup.sh
+++ PoolMaps/01_mpileup.sh
@@ -1,7 +1,9 @@
 REF='$HOME/pools/k55hyb_GUR2.fasta';
-SAMPLE='P13454_104';
+SAMPLE='P13454_103';
 
 # extract header, sort, select contigs > 200 bases, and create BED file
 samtools view -@ 14 -h ${SAMPLE}_trimmfastp_paired.wg.srtcrd.fix.mkdp.bam | \
 grep '^@SQ' | sort -V -k3 > "${SAMPLE}"_header.txt
 awk '{split($3,a,":"); if (a[2] > 200) {print $2,$3}}' "${SAMPLE}"_header.txt | sed -r 's/SN:|LN://g' > "${SAMPLE}"_selected.txt
+
+samtools mpileup -b mpileup_bamlist.txt -B -f $REF -o poolsALLdedup_L200.mpileup;
